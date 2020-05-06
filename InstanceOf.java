dd5cd86837467807a4dd99ca52ac564a589237e8
RiyaVijayan <63003471+RiyaVijayan@users.noreply.github.com>
2020-05-06 17:32:49 +0530

    Add files via upload

---
+++ InstanceOf.java
@@ -0,0 +1,11 @@
+//Use of instanceof operator in Java
+
+public class InstanceOf{
+
+   public static void main(String args[]) {
+
+      String name = "LGSI";
+      boolean result = name instanceof String;	//instance of returns true
+      System.out.println( result );
+   }
+} 
