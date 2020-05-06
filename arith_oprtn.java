f978b9519a56396b3e6b97880cc051a4c5b7435d
RiyaVijayan <63003471+RiyaVijayan@users.noreply.github.com>
2020-05-06 17:14:52 +0530

    Add files via upload

---
+++ arith_oprtn.java
@@ -0,0 +1,15 @@
+//arithmetic opertions in java
+public class arith_oprtn
+{  
+public static void main(String args[])
+{
+  
+int a=10;  
+int b=5;  
+System.out.println(a+b);  
+System.out.println(a-b);  
+System.out.println(a*b);  
+System.out.println(a/b);  
+System.out.println(a%b);  
+}
+}  
