4c0bfa382d99c46b4105976743a9843936502c43
RiyaVijayan <63003471+RiyaVijayan@users.noreply.github.com>
2020-05-06 18:40:30 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,15 @@
+// Java Program demonstrating Use of For loop - Pattern
+
+public class pattern 
+{  
+	public static void main(String[] args) 
+	{  
+		int term=6;  
+		for(int i=1;i<=term;i++){  
+			for(int j=1;j<=7-i;j++){  
+				System.out.print("* ");  
+			}  
+			System.out.println();  
+		}  
+	}  
+}  
