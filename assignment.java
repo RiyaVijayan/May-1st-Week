e4b180a3964dcc40e7061adc9322ca2bbd3e08ea
RiyaVijayan <63003471+RiyaVijayan@users.noreply.github.com>
2020-05-06 17:21:11 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,29 @@
+//assignment operations in java
+
+class assignment
+{  
+public static void main(String[] args)
+{  
+int a=10;  
+a+=3;  
+System.out.println(a);  
+a-=4;  
+System.out.println(a);  
+a*=2;  
+System.out.println(a);  
+a/=2;  
+System.out.println(a);  
+a%=4;  
+System.out.println(a);  
+a|=5;  
+System.out.println(a);  
+a&=4;  
+System.out.println(a);  
+a^=2;  
+System.out.println(a);  
+a>>=2;  
+System.out.println(a);  
+a<<=2;  
+System.out.println(a);  
+}
+}  
