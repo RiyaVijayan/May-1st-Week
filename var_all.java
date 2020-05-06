644f6b05138a2ed9e7f81e91a0d90c2fbb4d8686
RiyaVijayan <63003471+RiyaVijayan@users.noreply.github.com>
2020-05-06 17:04:37 +0530

    Add files via upload

---
+++ var_all.java
@@ -0,0 +1,15 @@
+/* Showing every variable types*/
+
+public class var_all{
+	public static void main(String[] args) {
+
+		int myNum = 5;
+		float myFloatNum = 5.99f;
+		char myLetter = 'D';
+		boolean myBool = true;
+		String myText = "Hello";
+		System.out.println("integer " + myNum + "\n" + "foat " + myFloatNum + "\n" + "character "+ myLetter + "\n" +"Boolean " + myBool + "\n" + "String " + myText + "\n");  
+	}
+}
+
+
